346f2ac4f8ad256ec46c797899027f84b006b039
IjosevitorI <138618362+IjosevitorI@users.noreply.github.com>
2024-11-07 02:01:25 +0000

    limpando erros bobos

--- Main.java
+++ Main.java
@@ -2,14 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
         Character shoto = new Character("Ryu", 1000);
-        Character shinobi = new Character("Ibuki", 1000);
+        Grappler grappler = new Grappler("Zangief", 1200);
+        Zoner zoner = new Zoner("Dhalsim", 1000);
 
         Match match = new Match();
 
-        match.fighting(shoto, shinobi);
+        match.fighting1(shoto, zoner);
+
+        match.fightingGrab(grappler, shoto);
+
+        match.fightingFireball(zoner, shoto);
 
         System.out.println("Vida do(a) " + shoto.getNome() + ": " + shoto.getLife());
-        System.out.println("Vida do(a) " + shinobi.getNome() + ": " + shinobi.getLife());
+        System.out.println("Vida do(a) " + grappler.getNome() + ": " + grappler.getLife());
+        System.out.println("Vida do(a) " + zoner.getNome() + ": " + zoner.getLife());
         
     }
 }
--- Match.java
+++ Match.java
@@ -3,7 +3,7 @@ public class Match{
     public Character shoto, shinobi;
 
     
-    public void fighting(Character shoto, Character shinobi){
+    public void fighting1(Character shoto, Character shinobi){
 
         action1 = shoto.lightpunch();
         action2 = shinobi.mediumpunch();
@@ -20,6 +20,29 @@ public class Match{
             shinobi.setLife(shinobi.getLife() - action1);
             shoto.setLife(shoto.getLife() - action2);
             System.out.println("ambos se acertaram");
+        }
+    }
+
+    public void fightingGrab(Grappler grappler, Character opponent) {
+        action1 = grappler.commandgrab();
+    
+        if (!opponent.isGrabBlocking()) {
+            opponent.setLife(opponent.getLife() - action1);
+            System.out.println(grappler.getNome() + " acertou um command grab!");
+        } 
+        else {
+            System.out.println(opponent.getNome() + " bloqueou o command grab de " + grappler.getNome());
+        }
+    }
+    
+    public void fightingFireball(Zoner zoner, Character opponent) {
+        action1 = zoner.fireball();
+        
+        if (!opponent.isBlocking()) {
+            opponent.setLife(opponent.getLife() - action1);
+            System.out.println(zoner.getNome() + " acertou uma fireball!");
+        } else {
+            System.out.println(opponent.getNome() + " bloqueou a fireball de " + zoner.getNome());
         }
     }
 }
